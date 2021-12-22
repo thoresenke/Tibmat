331e1c5e5bfd5c5e2b5f42cc9467097b71bc2441
Karl Erik Thoresen <karlerik.thoresen@nov.com>
2021-12-22 10:05:01 +0100

    trick to avoid checkin in the key

--- apikey.m
+++ apikey.m
@@ -1,6 +1,9 @@
 function [key] = apikey()
 %% [apikey] = apikey()
 % You find the api key here: https://developer.tibber.com/settings/accesstoken
+% Fill in the key and run the git command in the dos promt:
+%       git update-index --assume-unchanged apikey.m
+%  to avoid commiting the file
    key='';
    if isempty(key)
        error('you need to put in the apikey')
