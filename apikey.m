0a2791cb18517d5a48940cfc9654589589394f9f
Karl Erik Thoresen <karlerik.thoresen@nov.com>
2021-12-22 09:57:20 +0100

    .gitignore test

--- apikey.m
+++ apikey.m
@@ -1,7 +1,7 @@
 function [key] = apikey()
 %% [apikey] = apikey()
 % You find the api key here: https://developer.tibber.com/settings/accesstoken
-   key='0A';
+   key='';
    if isempty(key)
        error('you need to put in the apikey')
    end
