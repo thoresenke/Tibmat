12b488fda43fe2bb308c1e1551f93770c49de09a
Karl Erik Thoresen <karlerik.thoresen@nov.com>
2021-12-22 09:55:56 +0100

    Removed apikey from commit

--- apikey.m
+++ apikey.m
@@ -1,7 +1,7 @@
 function [key] = apikey()
 %% [apikey] = apikey()
 % You find the api key here: https://developer.tibber.com/settings/accesstoken
-   key='';
+   key='0A';
    if isempty(key)
        error('you need to put in the apikey')
    end
